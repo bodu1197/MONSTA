89a14cae8e6caf1cda9979856cd50507cde866f6
Your Name <your_email@example.com>
2025-10-24 18:05:31 +0700

    Feat: move middleware.ts to root to resolve Vercel warning

---
+++ middleware.ts
@@ -0,0 +1,4 @@
+// This file is a placeholder to satisfy Vercel's build process.
+// The actual middleware logic is in src/proxy.ts.
+export { config } from './src/proxy';
+export { proxy as default } from './src/proxy';
